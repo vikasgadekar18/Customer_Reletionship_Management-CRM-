9c09ea0a1e6f4d6f8a8e1744f2765cbe7c2b7a23
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-07-20 22:02:10 +0530

    initial commit

--- CrmProject-1/src/main/java/in/sp/main/controller/productsController.java
+++ CrmProject-1/src/main/java/in/sp/main/controller/productsController.java
@@ -1,13 +1,33 @@
 package in.sp.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import in.sp.main.entity.Product;
+
 
 @Controller
 public class productsController {
 	@GetMapping("/addproducts")
-	public String openaddproductpage() {
+	public String openaddproductpage(Model model) {
+		model.addAttribute("productAttr",new Product());
 		return "add-products";
+	}
+
+	@GetMapping("/addcourseform")
+	public String addcoursedetails(   
+			
+			 @ModelAttribute("productAttr")  Product product,
+			 @RequestParam("courseimage") MultipartFile courseimgurl,
+			 @RequestParam("trainersimage") MultipartFile trainerimgurl)
+	      
+     {
+		        
+		           return "";
 	}
 
 }
