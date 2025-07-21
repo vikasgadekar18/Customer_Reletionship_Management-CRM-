4cbffc41fb75dcc6660e98329c5c717c6960a3cd
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-07-21 21:16:34 +0530

    initial commit

---
+++ CrmProject-1/src/main/java/in/sp/main/service/ProductService.java
@@ -0,0 +1,12 @@
+package in.sp.main.service;
+
+import java.util.List;
+
+import in.sp.main.entity.Product;
+
+public interface ProductService {
+ public boolean addProductService(Product product);
+ public List<Product> getallproductsListService();
+	 
+ 
+}
