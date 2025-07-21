e42f112e9b8c970291a6b57013676c576b62c382
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-07-21 21:16:40 +0530

    initial commit

---
+++ CrmProject-1/src/main/java/in/sp/main/service/ProductServiceimpl.java
@@ -0,0 +1,32 @@
+package in.sp.main.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.sp.main.entity.Product;
+import in.sp.main.repository.ProductRepository;
+@Service
+public class ProductServiceimpl implements ProductService {
+    @Autowired
+	ProductRepository productRepository;
+	@Override
+	public boolean addProductService(Product product) {
+		boolean status = false;
+		try {
+			productRepository.save(product);
+			status=true;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return status;
+	}
+	@Override
+	public List<Product> getallproductsListService() {
+	   
+		return productRepository.findAll();
+	}
+
+}
