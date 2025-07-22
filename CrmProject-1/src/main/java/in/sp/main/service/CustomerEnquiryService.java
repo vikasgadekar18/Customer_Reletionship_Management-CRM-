a43a48e0f4b504d163effbce520ca079df6e2461
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-07-22 22:20:04 +0530

    initial commit

---
+++ CrmProject-1/src/main/java/in/sp/main/service/CustomerEnquiryService.java
@@ -0,0 +1,9 @@
+package in.sp.main.service;
+
+import in.sp.main.entity.CustomerEnquiry;
+
+public interface CustomerEnquiryService {
+
+	
+	public boolean addCustEnquiryDetailsService(CustomerEnquiry customerEnquiry);
+}
