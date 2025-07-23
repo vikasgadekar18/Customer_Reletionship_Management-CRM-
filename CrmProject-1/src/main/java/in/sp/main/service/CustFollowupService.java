1805401137e526ada8cd9f8c37fecf838059ef65
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-07-23 22:09:22 +0530

    initial commit

---
+++ CrmProject-1/src/main/java/in/sp/main/service/CustFollowupService.java
@@ -0,0 +1,7 @@
+package in.sp.main.service;
+
+import in.sp.main.entity.CustFollowup;
+
+public interface CustFollowupService {
+public boolean addCustFollwupDateService(CustFollowup custFollowup);
+}
