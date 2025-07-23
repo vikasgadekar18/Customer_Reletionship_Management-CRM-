e0af7d60cc67171903cf75b7102c05a2f622a502
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-07-23 22:09:12 +0530

    initial commit

---
+++ CrmProject-1/src/main/java/in/sp/main/entity/CustFollowup.java
@@ -0,0 +1,40 @@
+package in.sp.main.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="customer_folloup_date")
+public class CustFollowup {
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+    private int id;
+	@Column(name="phone_no")
+	private String phoneno;
+	@Column(name="followup_date")
+	private String followupdate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+	public String getFollowupdate() {
+		return followupdate;
+	}
+	public void setFollowupdate(String followupdate) {
+		this.followupdate = followupdate;
+	}
+	
+
+}
