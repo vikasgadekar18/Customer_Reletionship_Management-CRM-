11dc9e0f0856396737e54034765014da5e80e351
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-07-23 22:09:09 +0530

    initial commit

---
+++ CrmProject-1/src/main/java/in/sp/main/repository/CustfollowRepository.java
@@ -0,0 +1,10 @@
+package in.sp.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.sp.main.entity.CustFollowup;
+
+public interface CustfollowRepository extends JpaRepository<CustFollowup, Integer> {
+	
+
+}
