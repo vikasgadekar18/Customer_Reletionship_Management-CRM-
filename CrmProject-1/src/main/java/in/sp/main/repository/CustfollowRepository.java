78bbb8930a306bf9ec7bf1de0edcc762e47609b0
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-08-07 23:58:57 +0530

    follow ups updated

--- CrmProject-1/src/main/java/in/sp/main/repository/CustfollowRepository.java
+++ CrmProject-1/src/main/java/in/sp/main/repository/CustfollowRepository.java
@@ -1,5 +1,7 @@
 package in.sp.main.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import in.sp.main.entity.CustFollowup;
@@ -7,6 +9,7 @@ import in.sp.main.entity.CustFollowup;
 public interface CustfollowRepository extends JpaRepository<CustFollowup, Integer> {
 	
    CustFollowup findByPhoneno(String phoneno);
+   List<CustFollowup> findByFollowupdate(String followupdate);
    
    
 }
