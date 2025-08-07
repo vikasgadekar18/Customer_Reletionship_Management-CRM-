cf000657face91ed48ce254ec2380818f09a127e
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-08-07 23:58:16 +0530

    sale course updated

---
+++ CrmProject-1/src/main/java/in/sp/main/repository/SaleCourseRepository.java
@@ -0,0 +1,16 @@
+package in.sp.main.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import in.sp.main.entity.SaleCourse;
+
+public interface SaleCourseRepository  extends JpaRepository<SaleCourse, Integer>{
+	
+	@Query("SELECT date, COUNT(id) FROM SaleCourse GROUP BY date")
+	List<Object[]> countBypurchaseddate();
+	
+
+}
