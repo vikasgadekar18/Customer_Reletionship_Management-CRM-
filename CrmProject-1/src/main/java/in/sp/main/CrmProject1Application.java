a68f1caf409f9841808ec7ef947685795d29aeb1
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-08-16 22:43:55 +0530

    all controller updated

--- CrmProject-1/src/main/java/in/sp/main/CrmProject1Application.java
+++ CrmProject-1/src/main/java/in/sp/main/CrmProject1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrmProject1Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CrmProject1Application.class, args);
 		
